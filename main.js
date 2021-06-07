29c1c3bde6b25ef6acc10e0793ba85dea04fddbb
Pan-Grayza <67827867+Pan-Grayza@users.noreply.github.com>
2021-06-07 11:15:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("mobilemenu");
+let mobilenav = document.getElementById("mobilenav");
+
+mobilenav.style.left = "-100%";
+
+btn.onclick = function(){
+    if(mobilenav.style.left == "-100%"){
+        mobilenav.style.left = "50%";
+        btn.src = "images/icon-close.svg";
+    }
+
+    else{
+        mobilenav.style.left = "-100%";
+        btn.src = "images/icon-hamburger.svg";
+    }
+}
